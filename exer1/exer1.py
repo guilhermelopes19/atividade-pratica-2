6c113453e93cae4801dd1f8ccb506575545ef374
guilherme <guilherme.lopes0219@gmail.com>
2024-03-26 20:35:30 -0300

    Adicionado exercicios 1 e 2

---
+++ exer1/exer1.py
@@ -0,0 +1,7 @@
+#Alunos
+#Guilherme Souza Lopes - 072320015
+#Sara Stephanie Costa - 0723200
+
+#Imprimi no terminal a mensagem 'Alo mundo'
+
+print('Alo mundo')
---
+++ exer2/exer2.py
@@ -0,0 +1,10 @@
+#Alunos
+#Guilherme Souza Lopes - 072320015
+#Sara Stephanie Costa - 0723200
+
+nome1 = 'Sara Costa'
+nome2 = 'Guilherme Souza'
+matricula2 = '072320015'
+matricula1 = '0723200'
+
+print(f'Alo {nome1} - {matricula1}, {nome2} - {matricula2}')
